47ac2a846c88052ea6ce2606b62d8315dc11014e
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-01 17:40:13 -0300

    teste

---
+++ Juncao_nome.py
@@ -0,0 +1,10 @@
+nome = "Vinícius"
+sobren = "Pelizzari"
+nomec = nome + " " + sobren
+print(nomec)
+
+nome = "Vinícius"
+sobren = "Pelizzari"
+nomec = f"Olá, meu nome é {nome} {sobren}"
+print(nomec)
+print(type(nomec))
