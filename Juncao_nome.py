c250be578479cc5e72785640e18c294b8a2748b4
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-02 11:00:50 -0300

    Entrada com leitor de dados

--- Juncao_nome.py
+++ Juncao_nome.py
@@ -1,10 +1,17 @@
-nome = "Vinícius"
-sobren = "Pelizzari"
+print("Digite seu nome:")
+nome = input()
+print("Digite seu sobrenome:")
+sobren = input()
 nomec = nome + " " + sobren
 print(nomec)
 
-nome = "Vinícius"
-sobren = "Pelizzari"
-nomec = f"Olá, meu nome é {nome} {sobren}"
-print(nomec)
-print(type(nomec))
+#nome = "Vinícius"
+#sobren = "Pelizzari"
+#nomec = f"Olá, meu nome é {nome} {sobren}"
+#print(nomec)
+#print(type(nomec))
+
+#nome = "Vinícius"
+#sobren = "Pelizzari"
+#nomec = nome + " " + sobren
+#print(nomec)
