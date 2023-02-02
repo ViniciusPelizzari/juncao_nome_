bfc4cea61595a9efefc7b767bc568cfc2e9b8b60
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-02 15:09:36 -0300

    recolocacao código antigo

--- Juncao_nome.py
+++ Juncao_nome.py
@@ -10,3 +10,14 @@ nome = input()
 print("Digite seu sobrenome:")
 sobren = input()
 print(nome+" "+sobren)
+
+#nome = "Vinícius"
+#sobren = "Pelizzari"
+#nomec = f"Olá, meu nome é {nome} {sobren}"
+#print(nomec)
+#print(tipo(nomec))
+
+#nome = "Vinícius"
+#sobren = "Pelizzari"
+#nomec = nome + " " + sobrenome
+#print(nomec)
