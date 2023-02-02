a8488b07a5f37833189ce14bdbb9ea064fc36165
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-02 15:08:56 -0300

    Recolocação código antigo

--- Juncao_nome.py
+++ Juncao_nome.py
@@ -4,3 +4,9 @@ print("Digite seu sobrenome:")
 sobren = input()
 nomec = nome + " " + sobren
 print(nomec)
+
+print("Digite seu nome:")
+nome = input()
+print("Digite seu sobrenome:")
+sobren = input()
+print(nome+" "+sobren)
